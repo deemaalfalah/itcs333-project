30ef11ec897507f5e7c58b87a04695edb1984188
deemaalfalah <deema1719@gmail.com>
2024-11-25 18:57:05 +0300

    some change on  itcs333-project/view-instructor

---
+++ scripts/sidebar-toggle.js
@@ -0,0 +1,8 @@
+function toggleSubmenu(id) {
+    const submenu = document.getElementById(id);
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+    } else {
+        submenu.style.display = "block";
+    }
+}
