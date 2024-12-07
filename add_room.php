79f3726394768ef5a2c6939eb83ce2eb315e2566
AlyaShawqi <168932687+AlyaShawqi@users.noreply.github.com>
2024-12-07 21:56:23 +0300

    fix links in admin page

--- add_room.php
+++ add_room.php
@@ -97,10 +97,11 @@ if (isset($_SESSION['currentUser'])) {
                 <h2><?php echo htmlspecialchars($username); ?></h2>
             </div>
         <ul>
-            <li><a href="dashboard-admin.php">Dashboard</a></li>
-            <li><a href="add_room.php">Add Room</a></li>
-            <li><a href="edit-profile.php">My Account</a></li>
-            <li><a href="logout.php" class="logout-button">Logout</a></li>
+        <li><a href="dashboard-admin.php">Dashboard</a></li>
+                <li><a href="add_room.php">Add Room</a></li>
+                <li><a href="edit-profile-admin.php">My Account</a></li>
+                <li><a href="change-password-admin.php">Change Password</a></li>
+                <li><a href="logout.php" class="logout-button">Logout</a></li>
         </ul>
     </div>
 
